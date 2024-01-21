586155f97b4e1e39b0b980781387b4f53b9c8f5c
Shaun Hare <shaun@shaunhare.co.uk>
2024-01-21 17:31:51 +0000

    adding oidc role

--- cdk/index.ts
+++ cdk/index.ts
@@ -1,6 +1,7 @@
 import { Stack, App } from 'aws-cdk-lib';
 import { StaticSite } from "./stacks/s3-static-site-with-cloudfront";
 import { StateMachineStack } from './stacks/statemachine';
+import OidcConnection from './stacks/oidcrole';
 
 const app = new App();
 const stack = new Stack(app, "LincolnHack",{ env: {
@@ -20,5 +21,13 @@ new StateMachineStack(stack, "StateMachineStack", {
     domainName: "2024.lincolnhack.org",
 });
 
+new OidcConnection(stack, "OidcConnection", {
+    oidcProviderUrl: "https://token.actions.githubusercontent.com",
+    clientIds: ["sts.amazonaws.com"],
+    thumbprints: ["a031c46782e6e6c662c2c87c76da9aa62ccabd8e"],
+    repositoryConfig: [
+        { owner: "sdh100shaun", repo: "lincolnhack2024", filter: "main" },
+        ],
+      });
 
 app.synth();
