af3974c9ac52c592781f9b0ab99184cd2f170886
Shaun Hare <shaun@shaunhare.co.uk>
2024-02-04 20:00:30 +0000

    Feat cd workflow (#20)
    * initial tests for workflow
    
    * initial tests for workflow manual run option
    
    * initial tests for workflow manual run option
    
    * initial tests for workflow manual run option
    
    * initial tests for workflow manual run option
    
    * move to pr trigger to test
    
    * add id-token read
    
    * remove id-token
    
    * add id-token write permission
    
    * add id-token write permission
    
    * amended oidc role repo
    
    * added sha detials
    
    * updating dependabot
    
    * updating codeowners
    
    * updating dependabot yml
    
    * updating CD WORKFLOW
    
    * merge conflict|
    
    * amending workflow for npm i on cdk
    
    * amending workflow for deploy
    
    * amending workflow for deploy
    
    * added artefact bucket
    
    * added artefact bucket
    
    * added artefact bucket
    
    * added artefact bucket
    
    * added artefact bucket
    
    * fixing workflow
    
    * fixed perms
    
    * fixing workflow
    
    * fixing workflow
    
    * fixing workflow
    
    * fixing workflow
    
    * fixing workflow
    
    * fixing workflow region
    
    * fixing workflow region
    
    * fixing workflow region
    
    * fixing workflow for tagging

--- cdk/index.ts
+++ cdk/index.ts
@@ -2,6 +2,7 @@ import { App } from 'aws-cdk-lib';
 import { StaticSite } from "./stacks/s3-static-site-with-cloudfront";
 import { StateMachineStack } from './stacks/statemachine';
 import OidcConnection from './stacks/oidcrole';
+import { artefactBucket } from './stacks/artefact-bucket';
 
 const app = new App();
 
@@ -41,9 +42,16 @@ new OidcConnection(app, "OidcConnection", {
     oidcProviderUrl: "https://token.actions.githubusercontent.com",
     clientIds: ["sts.amazonaws.com"],
     thumbprints: ["a031c46782e6e6c662c2c87c76da9aa62ccabd8e"],
-    repositoryConfig: [
-          { owner: "sdh100shaun", repo: "2024.lincolnhack.org", filter: "ref:refs/heads/main" },
-        ],
+    repositoryConfig: 
+          { owner: "sdh100shaun", repo: "2024.lincolnhack.org", filter: "*" }
       });
+
+new artefactBucket(app, "artefactBucket", {
+  env: {
+    account: app.node.tryGetContext("account"),
+    region: app.node.tryGetContext("region"),
+  },
+  bucketname: "lincolnhack-artefacts",
+});
 
 app.synth();
