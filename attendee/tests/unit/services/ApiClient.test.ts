c15e7bef8489acc77ceddc9bc2c53762aacb27b9
Shaun Hare <shaun@shaunhare.co.uk>
2024-03-05 19:00:00 +0000

    unit tests for attendee

--- attendee/tests/unit/services/ApiClient.test.ts
+++ attendee/tests/unit/services/ApiClient.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, it, jest } from '@jest/globals';
 import { ApiClient } from '../../../src/services/ApiClient';
-import axios, { AxiosHeaders, AxiosInstance } from 'axios';
+import axios, { AxiosHeaders, AxiosInstance, AxiosError } from 'axios';
 
 
 // write tests for the ApiClient class
@@ -45,5 +45,25 @@ describe('ApiClient', function () {
     
         expect(axiosInstance.post).toHaveBeenCalledWith(endpoint, params, { signal });
         expect(result).toBe(response.data);
+    });
+
+    it('should call error when invalid response', () => {
+        // Arrange
+        const baseUrl = 'https://example.com';
+        const headers = new AxiosHeaders();
+        const authToken =   'authToken';
+        const endpoint = '/endpoint';
+        const params = { param: 'value' };
+        const signal = new AbortController().signal;
+        const axiosInstance = axios.create();
+        const axiosCreateMock = axiosInstance as jest.Mocked<AxiosInstance>;
+        jest.spyOn(axios, 'create').mockReturnValue(axiosCreateMock);
+        const apiClient = new ApiClient(baseUrl, headers, authToken);
+        jest.spyOn(apiClient, 'createClient').mockReturnValue(axiosInstance);
+        jest.spyOn(axiosInstance, 'get').mockRejectedValue(new AxiosError('Request failed'));
+        apiClient.get(endpoint, params, signal).catch((error) => {
+            expect(error).toBeInstanceOf(Error);
+            expect(error.message).toBe('Request failed: Request failed');
+        });
     });
 });
