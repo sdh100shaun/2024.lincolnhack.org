9b0bf3ad4b1070224b5fe6db2d863c6e08cfb4b3
Shaun Hare <shaun@shaunhare.co.uk>
2024-03-17 15:16:58 +0000

    added slack invite

--- attendee/src/services/Slack.ts
+++ attendee/src/services/Slack.ts
@@ -1,1 +1,17 @@
-// use slack bolt to create a new invite app
+import { WebClient } from '@slack/web-api';
+
+const token: string = (apiKey?: string) => {
+    apiKey || process.env.SLACK_API_TOKEN;
+};
+
+const web = new WebClient(token);
+
+async function inviteNewUser(email: string, channel_ids: string, team_id: string) {
+    try {
+        await web.admin.users.invite({ email, channel_ids, team_id });
+        console.log(`Successfully invited new user with email ${email}`);
+    } catch (error) {
+        console.error(`Error inviting new user with email ${email}:`, error);
+    }
+}
+export default inviteNewUser;
