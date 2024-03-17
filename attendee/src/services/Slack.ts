976c6a8a19b12c131b6597c79c1a6ceab436ca6e
Shaun Hare <shaun@shaunhare.co.uk>
2024-03-17 18:38:34 +0000

    amended slack service

--- attendee/src/services/Slack.ts
+++ attendee/src/services/Slack.ts
@@ -1,1 +1,24 @@
-// use slack bolt to create a new invite app
+import { WebClient } from '@slack/web-api';
+
+const token: string = getApiKey();
+
+const getApiKey = () => {
+    const apiKey = process.env.SLACK_API_TOKEN;
+    if (!apiKey) {
+        console.error('SLACK_API_TOKEN is not set');
+        return null;
+    }
+    return apiKey;
+};
+
+const web = new WebClient(token);
+
+async function inviteNewUser(email: string, channel_ids: string, team_id: string) {
+    try {
+        await web.admin.users.invite({ email, channel_ids, team_id });
+        console.log(`Successfully invited new user with email ${email}`);
+    } catch (error) {
+        console.error(`Error inviting new user with email ${email}:`, error);
+    }
+}
+export default inviteNewUser;
