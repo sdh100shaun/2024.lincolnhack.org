b226b4197073c7d2212cb8b66b97fec85e709956
Shaun Hare <shaun@shaunhare.co.uk>
2024-03-18 15:44:51 +0000

    WIP for agenda

--- astro-site/src/scripts/agenda.ts
+++ astro-site/src/scripts/agenda.ts
@@ -1,20 +1,27 @@
 // Purpose: Agenda functionality for the site
-let currentTimestamp = new Date().getTime();
-
 const agenda = () => {
     const agendaItems = document.querySelectorAll('.event__time');
-    
+    let currentTimestamp = new Date().getTime();
     if (agendaItems.length > 0) {
         agendaItems.forEach((item) => {
-        console.log(item.dataset.starttime);
-        const itemTimestamp = parseInt(item.dataset.starttime);
         
-        if (itemTimestamp > currentTimestamp) {
-            item.classList.add('agenda__item--past');
-            console.log('past');
-        }
-        });
+            const difference = new Date(parseInt(item.dataset.starttime)).getTime() - currentTimestamp;
+            var secondsDifference = Math.floor(difference/1000);
+            console.log(secondsDifference);
+            if (secondsDifference > 0 && secondsDifference < 3600) {
+                item.classList.add('agenda__item--past');
+                item.innerHTML = 'been and gone';
+            }
+            else if (secondsDifference > 0 && secondsDifference < 3600) {
+                item.classList.add('agenda__item--current');
+                item.innerHTML = ' &#128640; happening now';
+            }
+            else {
+                item.classList.add('agenda__item--future');
+                item.innerHTML = 'starting in ' + Math.floor(secondsDifference/3600) + ' hours';
+            }
+        })
     }
 };
-
-const interval = setInterval(agenda, 1000);
+agenda();
+const interval = setInterval(agenda, 1000,1);
